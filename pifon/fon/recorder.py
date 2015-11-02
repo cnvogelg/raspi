c24f57d7eb9763a2ea5d9effbf9001aea2d78220
Christian Vogelgsang <C.Vogelgsang@web.de>
2015-11-02 20:46:00 +0100

    use rate file

--- pifon/fon/recorder.py
+++ pifon/fon/recorder.py
@@ -1,9 +1,33 @@
+from __future__ import print_function
 import subprocess
+import os
+import sys
 
 class Recorder:
+  rate_file = "~/.sample_rate"
+
   def __init__(self):
-    cmd = ["tools/vumeter"]
+    rate = self._get_rate()
+    if rate is None:
+      print("No rate file '%s'! Using default..." % self.rate_file, file=sys.stderr)
+      rate = 22050
+    cmd = ["tools/vumeter", str(rate)]
     self.p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE)
+
+  def _get_rate(self):
+    # try to get sample rate
+    try:
+      rate_file = os.path.expanduser(self.rate_file)
+      if os.path.exists(rate_file):
+        fh = open(rate_file)
+        l = fh.readline()
+        l = l.strip()
+        rate = int(l)
+        fh.close()
+        return rate
+    except:
+      pass
+    return None
 
   def read_rms(self):
     """read the next rms value"""
--- pifon/mon/parser.py
+++ pifon/mon/parser.py
@@ -2,21 +2,25 @@ from __future__ import print_function
 import sys
 
 class Parser:
-  
+
   def __init__(self, state):
     self.state = state
-  
-  def _warn(self, *args):
-    print("parser?:",*args,file=sys.stderr)
-    
-  def audio_state(self,*args):
+
+  def _warn(self, msg):
+    print("parser?:",msg,file=sys.stderr)
+
+  # pifon/audio calls
+
+  def audio_state(self, msg):
+    args = msg.args
     if len(args) == 2:
       state = args[1]
       self.state.report_audio_state(state)
     else:
       self._warn(args);
-    
-  def audio_level(self,*args):
+
+  def audio_level(self, msg):
+    args = msg.args
     if len(args) == 3:
       try:
         max_level = int(args[1])
@@ -26,73 +30,90 @@ class Parser:
         self._warn(args)
     else:
       self._warn(args)
-    
-  def audio_pong(self,*args):
+
+  def audio_pong(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.report_audio_pong()
     else:
       self._warn(args)
-    
-  def mute(self,*args):
+
+  # external commands
+
+  def mute(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_audio_mute(True,True)
     else:
       self._warn(args)
-  
-  def unmute(self,*args):
+
+  def unmute(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_audio_mute(False,True)
     else:
       self._warn(args)
 
-  def listen(self,*args):
+  def listen(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_audio_listen(True,True)
     else:
       self._warn(args)
-  
-  def unlisten(self,*args):
+
+  def unlisten(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_audio_listen(False,True)
     else:
       self._warn(args)
 
-  def chime(self,*args):
+  def chime(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_audio_chime(True,True)
     else:
       self._warn(args)
-  
-  def nochime(self,*args):
+
+  def nochime(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_audio_chime(False,True)
     else:
       self._warn(args)
 
-  def blank(self,*args):
+  def blank(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_blank(True,True)
     else:
       self._warn(args)
-  
-  def noblank(self,*args):
+
+  def noblank(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.execute_blank(False,True)
     else:
       self._warn(args)
-  
-  def connected(self,*args):
+
+  # xmppbot callbacks
+
+  def connected(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.connected()
     else:
       self._warn(args)
-    
-  def disconnected(self,*args):
+
+  def disconnected(self, msg):
+    args = msg.args
     if len(args) == 1:
       self.state.disconnected()
     else:
       self._warn(args)
-  
+
+  # dispatcher
+
   valid_commands = {
     'audio_state' : audio_state,
     'audio_level' : audio_level,
@@ -108,14 +129,14 @@ class Parser:
     'blank' : blank,
     'noblank' : noblank
   }
-  
-  def dispatch(self, args):
-    if len(args) == 0:
+
+  def dispatch(self, msg):
+    if len(msg.args) == 0:
       return False
-    cmd = args[0]
+    cmd = msg.args[0]
     for a in self.valid_commands:
       if a == cmd:
         f = self.valid_commands[a]
-        return f(self, *args)
-    print("huh? ",args,file=sys.stderr)
+        return f(self, msg)
+    print("huh? ",msg,file=sys.stderr)
     return False
